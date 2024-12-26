46593bb0a561cafa6a4de84ff5f1eb86b6e32b83
daltondiaz <d_tecnologia@yahoo.com.br>
2024-12-26 14:24:07 -0300

    aoc 2024, day01 part1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,48 +1,45 @@
-use std::{fs::File, io::{self, BufRead, BufReader}};
+use std::{
+    fs::File, i32, io::{self, BufRead, BufReader}
+};
 
-pub fn solution() -> io::Result<()>{
-    //let mut file = File::open("input/day01/data.in")?;
-    /*
-    * TODO add this in one file separeted to be reusable
-    */
-    let file = File::open("input/day01/demo1.in")?;
-    let reader = BufReader::new(file);
+pub fn solution(file: io::Result<File>) -> io::Result<i32> {
+    let reader = BufReader::new(file?);
 
     let mut left: Vec<i32> = Vec::new();
     let mut right: Vec<i32> = Vec::new();
 
-    for line in reader.lines(){
+    for line in reader.lines() {
         let line = line?;
-        println!("line: {}", line);
         let parts: Vec<&str> = line.split_whitespace().collect();
-        println!("left");
-        left = sort(parts[0].parse().unwrap(), left);
-        println!("right");
-        right = sort(parts[1].parse().unwrap(), right);
+        let l: i32 = parts[0].parse().unwrap();
+        let r: i32 = parts[1].parse().unwrap();
+        //println!("line: {} \t left {} \t right {}", line, l, r);
+        left = sort(l, left);
+        //println!("left arr: {:?}", left);
+        right = sort(r, right);
+        //println!("right arr: {:?}", right);
     }
 
-    println!("{:?}", left);
-    println!("{:?}", right);
-
-    Ok(())
+    let mut sum :i32 = 0;
+    for i in 0..right.len() {
+        sum += i32::abs(right[i]-left[i])
+    }
+    Ok(sum)
 }
-
-fn sort(val:i32,  arr:Vec<i32>) -> Vec<i32> {
-    let mut new_arr: Vec<i32> = Vec::new();
-    if arr.len() == 0 {
-        println!("empty {}", val);
-        new_arr.push(val);
-        return new_arr
-    }
-    for value in arr {
-        if val >= value{
-            println!("value {}", value);
-            new_arr.push(value);
-        } else {
-            println!("val {}", val);
-            new_arr.push(val);
+/*
+* I implemented this sort only to learn more about rust and practice!
+*/
+fn sort(val: i32, mut arr: Vec<i32>) -> Vec<i32> {
+    arr.push(val);
+    //print!("value of array: ");
+    let len = arr.len();
+    for i in 0..len {
+        for j in 0..len- i -1{
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j+1);
+            }
         }
     }
-    new_arr
+    //println!();
+    arr
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use std::fs::File;
+
 mod days;
 
 fn main() {
     println!("AOC 2024 in Rust");
-    days::day01::solution();
+    let file = File::open("input/day01/demo1.in");
+    let day_01_demo = days::day01::solution(file).unwrap();
+    println!("Day01\t demo:{day_01_demo}");
+    let file = File::open("input/day01/data.in");
+    let day_01_part_01 = days::day01::solution(file).unwrap();
+    println!("Day01\t first part:{day_01_part_01}");
 }
